e009f3becefb2e6f41e44c3462fbf6c2c4aead94
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-04-28 15:44:39 -0400

    database dot js file added for handling tnew transaction entries  when internet connection is detected again after being lost

---
+++ public/database.js
@@ -0,0 +1,71 @@
+let db;
+let budgeVer;
+
+const request = indexedDB.open('budget', budgeVer || 3);
+
+request.onupgradeneeded = event => {
+    console.log('Upgrade needed in IndexDB');
+
+    const { budgeFormer } = e;
+    const budgeFresh = e.budgeFresh || db.version;
+
+    console.log(`db version from ${budgeFormer} to ${budgeFresh} ✓`);
+
+    db = event.target.result;
+
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetStore', { autoIncrement: true });
+    }
+};
+
+request.onerror = event => {
+    console.log(`oh dear. ${event.target.errorCode}`);
+};
+
+function checkDatabase() {
+    console.log('db checked ✓');
+
+    let transaction = db.transaction(['BudgetStore'], 'readwrite');
+    const store = transaction.objectStore('BudgetStore');
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then((res) => {
+                    if (res.length !== 0) {
+                        transaction = db.transaction(['BudgetStore'], 'readwrite');
+                        const currentStore = transaction.objectStore('BudgetStore');
+                        currentStore.clear();
+                        console.log('stored transactions cleared ✓');
+                    }
+                });
+        }
+    };
+}
+
+request.onsuccess = function (e) {
+    console.log('noice ✓');
+    db = e.target.result;
+    if (navigator.onLine) {
+        console.log('backend online ✓');
+        checkDatabase();
+    }
+};
+
+const saveRecord = (record) => {
+    console.log('✓ saveRecord ✓');
+    const transaction = db.transaction(['BudgetStore'], 'readwrite');
+    const store = transaction.objectStore('BudgetStore');
+    store.add(record);
+};
+
+window.addEventListener('online', checkDatabase);
